683c62a8f2e1ae5fdf12ac6d0713ba10f958bbd8
anggakharisma <anggakharisma@protonmail.com>
2024-10-24 12:23:18 +0700

    chore: support decimal

--- api/models/food.go
+++ api/models/food.go
@@ -1,12 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
 
 type Food struct {
 	gorm.Model
-	Name   string   `json:"name"`
-	Image  string   `json:"image"`
-	Price  float64  `json:"price"`
-	Stock  int      `gorm:"default:100" json:"stock"`
-	Extras []*Extra `gorm:"many2many:food_extra;" json:"extras"`
+	Name   string          `json:"name"`
+	Image  string          `json:"image"`
+	Price  float64         `json:"price"`
+	Stock  decimal.Decimal `gorm:"default:500" json:"stock" gorm:"type:deicmal(7, 6)"`
+	Extras []*Extra        `gorm:"many2many:food_extra;" json:"extras"`
 }
--- api/requests/food_request.go
+++ api/requests/food_request.go
@@ -6,11 +6,11 @@ import (
 
 type FoodRequest struct {
 	Name  string               `form:"name" binding:"required"`
-	Price int                  `form:"price" binding:"required"`
+	Price float64              `form:"price" binding:"required"`
 	Image multipart.FileHeader `form:"image" binding:"required"`
 }
 
 type UpdateFoodInput struct {
-	Name  string `json:"name"`
-	Price int    `json:"price"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
