89ce3b65ef732738d433b2833c4c491225c8e2ca
Angga <anggakharisma@protonmail.com>
2025-10-10 18:48:23 +0700

    chore: cleaning up

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,7 @@
 "use client"
 
-import { useRouter } from "next/router"
-import { useEffect } from "react"
 
 export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push("/orders")
-  }, [router])
   return (
     <div></div>
   )
